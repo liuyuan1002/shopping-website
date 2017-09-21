d26f36f200c824294193c5c7f60bbba279bc77c9
liuyuan <457868671@qq.com>
2017-09-21 17:54:58 +0800

    2017092102

--- text.py
+++ text.py
@@ -19,7 +19,7 @@ width = 60 * 4
 height = 60
 image = Image.new('RGB', (width, height), (255, 255, 255))
 # 创建Font对象:
-font = ImageFont.truetype(r'E:\workspace\PycharmProjects\shopping-website\Arial.ttf', 36)
+font = ImageFont.truetype(r'/home/ubuntu/shopping-website/Arial.ttf', 36)
 # 创建Draw对象:
 draw = ImageDraw.Draw(image)
 # 填充每个像素:
