34756566bea6991ca15bf413b0c1dd41f1955398
liuyuan <457868671@qq.com>
2017-10-31 17:27:35 +0800

    20171031blog

---
+++ wwwroot/config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '11111111');
+define('UC_DBNAME', 'x25gbk');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`x25gbk`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', '30a95e43a171f59916a0c59d9718543f1fe51b5338811fa1c0f2e2de50ffdc01');
+define('UC_API', 'http://127.0.0.1/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
